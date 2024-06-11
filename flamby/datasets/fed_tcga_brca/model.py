f2f53d405c25b51bbc32d957af70034bc2b0e690
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2024-06-11 13:59:27 +0200

    Update model.py

--- flamby/datasets/fed_tcga_brca/model.py
+++ flamby/datasets/fed_tcga_brca/model.py
@@ -14,55 +14,25 @@ class Baseline(nn.Module):
         input_dim = 39
         output_dim = 1
         self.dropout = dropout
-        self.fc = nn.Linear(input_dim, output_dim)
-
-        self.layer1 = nn.Linear(input_dim, int(input_dim/2))
-        self.dropout_layer = nn.Dropout(p=self.dropout)
-        self.relu = nn.ReLU()
-        self.layer2 = nn.Linear(int(input_dim/2), output_dim)
+        if self.dropout == 0:
+            self.fc = nn.Linear(input_dim, output_dim)
+        else:
+            self.fc1 = nn.Linear(input_dim, 32)
+            self.fc2 = nn.Linear(32, 64)
+            self.fc3 = nn.Linear(64, 32)
+            self.fc4 = nn.Linear(32, 1)
+            self.act = nn.LeakyReLU()
+            self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         if self.dropout == 0:
             return self.fc(x)
         else:
-            x = self.relu(self.layer1(x))
-            x = self.dropout_layer(x)
-            x = self.layer2(x)
+            x = x.type(torch.float32)
+            x = self.act(self.fc1(x))
+            x = self.dropout(x)
+            x = self.act(self.fc2(x))
+            x = self.dropout(x)
+            x = self.act(self.fc3(x))
+            x = self.fc4(x)
             return x
-
-
-if __name__ == "__main__":
-
-    mydataset = FedTcgaBrca(train=True, pooled=True)
-
-    model = Baseline()
-
-    for i in range(10):
-        X = torch.unsqueeze(mydataset[i][0], 0)
-        y = torch.unsqueeze(mydataset[i][1], 0)
-        print(X.shape)
-        print(y)
-        print(model(X))
-
-
-
-'''
-    def __init__(self, dropout, input_dim=13, output_dim=1):
-        super(Baseline, self).__init__()
-        self.dropout = dropout
-        self.linear = nn.Linear(input_dim, output_dim)
-        self.layer1 = nn.Linear(input_dim, int(input_dim/2))
-        self.dropout_layer = nn.Dropout(p=self.dropout)
-        self.relu = nn.ReLU()
-        self.layer2 = nn.Linear(int(input_dim/2), output_dim)
-
-    def forward(self, x):
-        if self.dropout == 0:
-            return torch.sigmoid(self.linear(x))
-        else:
-            x = self.relu(self.layer1(x))
-            x = self.dropout_layer(x)
-            x = self.layer2(x)
-            return torch.sigmoid(x)
-
-'''
