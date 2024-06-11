4317a36af61fcad178d3406078ea0f110e586fb0
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2024-06-11 13:48:40 +0200

    Update model

--- flamby/datasets/fed_isic2019/model.py
+++ flamby/datasets/fed_isic2019/model.py
@@ -34,36 +34,13 @@ class Baseline(nn.Module):
         self.output = nn.Sequential(nn.LeakyReLU(),
                                     nn.Dropout(p=dropout),
                                     nn.Linear(256, 8))
-
+        self.relu = nn.ReLU()
+        self._dropout = nn.Dropout(p=dropout)
+        
     def forward(self, image):
         out = self.base_model(image)
-        out = self.output(out)
+
+        out = self._dropout(out)
+        out = self.relu(out)
+        out = self._final_fc(out)
         return out
-
-
-if __name__ == "__main__":
-
-    sz = 200
-    train_aug = albumentations.Compose(
-        [
-            albumentations.RandomScale(0.07),
-            albumentations.Rotate(50),
-            albumentations.RandomBrightnessContrast(0.15, 0.1),
-            albumentations.Flip(p=0.5),
-            albumentations.Affine(shear=0.1),
-            albumentations.RandomCrop(sz, sz),
-            albumentations.CoarseDropout(random.randint(1, 8), 16, 16),
-            albumentations.Normalize(always_apply=True),
-        ]
-    )
-
-    mydataset = FedIsic2019(0, True, "train", augmentations=train_aug)
-
-    model = Baseline()
-
-    for i in range(50):
-        X = torch.unsqueeze(mydataset[i][0], 0)
-        y = torch.unsqueeze(mydataset[i][1], 0)
-        print(X.shape)
-        print(y.shape)
-        print(model(X))
